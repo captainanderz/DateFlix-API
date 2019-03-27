61a9196c70a0eaa620e7aa879cb74ff394cc148c
captainanderz <anders.lindhardt999@gmail.com>
2019-03-27 11:42:48 +0100

    Removed unused UserId property from UserDetail class

--- Dateflix-MVC/Models/SignalR/UserDetail.cs
+++ Dateflix-MVC/Models/SignalR/UserDetail.cs
@@ -8,7 +8,6 @@ namespace DateflixMVC.Models.SignalR
     public class UserDetail
     {
         public string ConnectionId { get; set; }
-        public int UserID { get; set; }
         public string Email { get; set; }
     }
 }
