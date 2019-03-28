2532e2272828022ad0b8e7ad6ac7de70a9e6fc18
captainanderz <anders.lindhardt999@gmail.com>
2019-03-28 11:03:01 +0100

    Refactoring

--- Dateflix-MVC/Extensions/UserDetailExtension.cs
+++ Dateflix-MVC/Extensions/UserDetailExtension.cs
@@ -17,23 +17,6 @@ namespace DateflixMVC.Extensions
             return userDetailsList;
         }
 
-        public static List<UserDetail> AddConnectionIdToUser(this List<UserDetail> userDetailsList, string email, string connectionId)
-        {
-            var userToUpdate = userDetailsList.FirstOrDefault(x => x.Email == email);
-
-            if (userToUpdate != null)
-            {
-                userToUpdate.ConnectionId = connectionId;
-            }
-
-            return userDetailsList;
-        }
-
-        public static bool HasEmail(this List<UserDetail> userDetailsList, string email)
-        {
-            return userDetailsList.Any(x => x.Email.ToLower() == email.ToLower());
-        }
-
         public static UserDetail GetUserByConnectionId(this List<UserDetail> userDetailsList, string connectionId)
         {
             return userDetailsList.FirstOrDefault(x => x.ConnectionId == connectionId);
@@ -41,10 +24,10 @@ namespace DateflixMVC.Extensions
 
         public static void RemoveUser(this List<UserDetail> userDetailsList, string connectionId)
         {
-            var key = userDetailsList.GetUserByConnectionId(connectionId);
-            if (key != null)
+            var user = userDetailsList.GetUserByConnectionId(connectionId);
+            if (user != null)
             {
-                userDetailsList.Remove(key);
+                userDetailsList.Remove(user);
             }
         }
     }
